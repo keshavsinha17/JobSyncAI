4bce037a12fd9209c2610d7c2190bdbaf2fdc51c
“[keshav <123925652+keshavsinha17@users.noreply.github.com>
2025-02-28 01:42:47 +0530

    User controller added

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -1,10 +1,37 @@
 import mongoose from "mongoose"
 
-const googleLogin=(req,res)=>{
+const googleLogin=async (req,res)=>{
     try {
-        
+        const code = req.query;
+        const googleRes=await oauth2Client.getToken(code);
+        oauth2Client.setCredentials(googleRes.tokens)
+        const userRes=await axios.get("https://www.googleapis.com/oauth2/v1/userinfo",{
+            headers:{Authorization:`Bearer ${googleRes.tokens.access_token}`}
+        })
+        const{email,name,picture}=userRes.data;
+        let user = await User.findOne({email});
+        if(!user){
+            user=await User.create({
+                name,
+                email,
+                image:picture
+            })
+        }
+        const token = jwt.sign({_id,email},
+            process.env.JWT_SECRET,{
+            expiresIn:process.env.JWT_EXPIRES_IN
+        });
+        return res.status(200).json({
+            status:"success",
+            user,
+            token
+        })
+    
     } catch (error) {
-        
+        res.status(500).json({
+            status:"error",
+            message:"Something went wrong"
+        })
     }
 }
 export {googleLogin}
